c505e97503d074859ebbab8c033a2946cb77c01a
lmpanalytics2 <lmpconsulting@hotmail.com>
2018-07-13 11:32:05 +0200

    initial commit

---
+++ src/main/java/com/lmp/visualizer/visualizer1/VisualizerMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lmp.visualizer.visualizer1;
+
+/**
+ *
+ * @author SEPALMM
+ */
+public class VisualizerMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
